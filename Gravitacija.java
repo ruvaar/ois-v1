4b47b2354a891b51089f8eace6b63d212518eef8
akFGH <anita.koprivc1@gmail.com>
2022-02-26 20:59:01 +0100

    Dodan izračun v Grav

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Gravitacija {
     public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		double nadmorskaVisina = sc.nextDouble();
+        double masaZemlje = 5972000000000000000000000.0;
+        double polmerZemlje = 6371000.0;
+        double gravitacijskaKonstanta = 0.00000000006674;
+        double visinaPlusPolmer = nadmorskaVisina + polmerZemlje;
+
+        double gravitacijskiPospesek = (gravitacijskaKonstanta * masaZemlje) / Math.pow(visinaPlusPolmer, 2);
+		
         System.out.println("OIS je zakon!");
     }
 }
